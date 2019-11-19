90f7f44a3852cfeda01275424804c27aaf5e96e0
인공지능 <net94.teacher@gmail.com>
2019-11-19 12:44:23 +0900

    bubble_sort 구현 및 swap적용(김승영)

--- chapter04/exam02.py
+++ chapter04/exam02.py
@@ -1,9 +1,18 @@
 import random as rnd
 
 
+def swap(random_list, i, j):
+    random_list[i], random_list[j] = random_list[j], random_list[i]
+    return random_list
+
 def bubble_sort(random_list):
-    print(random_list)
-    # todo
+    for cnt in range(len(random_list)-1):
+        for i in range(len(random_list)-1):
+            if random_list[i] > random_list[i+1]:
+                # random_list[i], random_list[i+1] = random_list[i+1], random_list[i]
+                random_list = swap(random_list, i, i+1)
+        print("bubble_sort(random_list) => {}".format(random_list))
+    return random_list
 
 
 if __name__ == "__main__":
