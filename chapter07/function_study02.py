0f3ed51c4cdb8317346fcdb5e2f0ebcb926cbdaf
인공지능 <net94.teacher@gmail.com>
2019-11-21 12:32:50 +0900

    함수를 이용하여 학생관리 추가

---
+++ chapter07/function_study02.py
@@ -0,0 +1,11 @@
+"""
+list, tuple, dictionary, set 을 이용하여 아래 프로그램 각각 을 작성하시오.
+
+student_management 패키지를 추가
+
+1. 학생목록, 2. 학생 추가, 3. 학생 수정, 4. 학생 삭제, 5. 종료 메뉴 추가
+2. 프로그램 수행 시 먼저 student_list.txt 파일을 읽어와 수행
+3. 각각 메뉴별 수행되도록 작성
+4. 종료 시 학생목록이 student_list.txt에 저장
+
+"""
